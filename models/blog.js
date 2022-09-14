6c234693cb843d97f72148d3d3dca2e4b8cead04
Niru Magar <nirumagar901@gmail.com>
2022-09-14 13:34:02 +0545

    chore: connecting to database through model

---
+++ models/blog.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+// making schema
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+//to remove -id,--v
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
