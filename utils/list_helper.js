1e4484d881cf432290d3e961d2689363cf68d671
Niru Magar <nirumagar901@gmail.com>
2022-09-15 15:17:34 +0545

    chore: passing totalLikes tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (like) => {
+  console.log("hi", like);
+
+  return like.reduce((a, b) => a + b.likes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
