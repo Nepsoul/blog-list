23f1c2916a529523a43bfc0ab68e7359b9944dd8
Niru Magar <nirumagar901@gmail.com>
2022-09-16 13:47:46 +0545

    chore: test case of favoriteBlog

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -8,6 +8,22 @@ test("dummy returns one", () => {
 });
 
 describe("total likes", () => {
+  test("when list has only one blog, equals the likes of that", () => {
+    const listWithOneBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+});
+test("when list has more than one blog, equals the likes of that", () => {
   const listWithOneBlog = [
     {
       _id: "5a422aa71b54a676234d17f8",
@@ -17,10 +33,33 @@ describe("total likes", () => {
       likes: 5,
       __v: 0,
     },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    },
   ];
+  const result = listHelper.totalLikes(listWithOneBlog);
+  expect(result).toBe(17);
+});
 
-  test("when list has only one blog, equals the likes of that", () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
-    expect(result).toBe(5);
+describe("favorite Blog", () => {
+  const mostLikes = [
+    {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    },
+  ];
+  test("when blog has most likes, equals the likes of that", () => {
+    const result = listHelper.favoriteBlog(mostLikes);
+    expect(result).toStrictEqual({
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    });
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,12 +3,43 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (like) => {
-  console.log("hi", like);
-
+  // console.log("hi", like);
   return like.reduce((a, b) => a + b.likes, 0);
+};
+
+const favoriteBlog = (mostLike) => {
+  console.log("highlike", mostLike);
+  let highlike = mostLike.reduce((preVal, curVal) => {
+    if (mostLike.length === 1) {
+      console.log(mostLike.length);
+      preVal = {
+        title: curVal.title,
+        author: curVal.author,
+        likes: curVal.likes,
+      };
+    } else {
+      if (preVal.likes < curVal.likes) {
+        preVal = {
+          title: curVal.title,
+          author: curVal.author,
+          likes: curVal.likes,
+        };
+      } else {
+        preVal = {
+          title: curVal.title,
+          author: curVal.author,
+          likes: curVal.likes,
+        };
+      }
+    }
+    console.log("val", preVal);
+    return preVal;
+  }, {});
+  return highlike;
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
