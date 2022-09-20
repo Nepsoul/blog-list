3ca499d7fb4c480b29de9bebb7276c0ac5f15efe
Niru Magar <nirumagar901@gmail.com>
2022-09-20 11:45:50 +0545

    chore: refactor

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,49 +8,16 @@ const totalLikes = (like) => {
 };
 
 const favoriteBlog = (mostLike) => {
-  console.log("highlike", mostLike);
-  let highlike = mostLike.reduce((preVal, curVal) => {
-    if (mostLike.length === 1) {
-      console.log(mostLike.length);
-      preVal = {
-        title: curVal.title,
-        author: curVal.author,
-        likes: curVal.likes,
-      };
+  let topLikes = 0;
+  let topBlog = {};
+  for (let i = 0; i < mostLike.length; i++) {
+    if (mostLike[i].likes > topLikes) {
+      topLikes = mostLike[i].likes;
+      topBlog = mostLike[i];
     }
-    if (preVal.likes >= curVal.likes) {
-      console.log("else condn", preVal.likes);
-      preVal = {
-        title: preVal.title,
-        author: preVal.author,
-        likes: preVal.likes,
-      };
-    } else {
-      preVal = {
-        title: curVal.title,
-        author: curVal.author,
-        likes: curVal.likes,
-      };
-    }
-
-    console.log("val", preVal);
-    return preVal;
-  }, {});
-  return highlike;
-};
-
-/*==>alternative<====
-const favoriteBlog = (blogs) => {
-   let topLikes = 0;
-   let topBlog = {};
-   for (let i = 0; i < blogs.length; i++) {
-    if (blogs[i].likes > topLikes) {
-       topLikes = blogs[i].likes;
-      topBlog = blogs[i];
-    }
-   }
+  }
   return topBlog;
- };*/
+};
 
 module.exports = {
   dummy,
