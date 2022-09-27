2f443a811bb11c545aafb2e55c34ebe80ae7864c
Niru Magar <nirumagar901@gmail.com>
2022-09-27 16:49:38 +0545

    feat: setting up different path for test

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 
 //handaling env variable extracted from seperate file
-const PORT = process.env.PORT; //port defining in env to 3001.
-const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT; //port defining in env to 3003
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = {
   MONGODB_URI,
