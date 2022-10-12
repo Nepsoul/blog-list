e17988d19413e43ddf8de8e9d463f6f16b0b45d6
Niru Magar <nirumagar901@gmail.com>
2022-10-12 20:15:16 +0545

    chore: adding condn of username, ps length less than 3

--- controllers/users.js
+++ controllers/users.js
@@ -2,28 +2,37 @@ const bcryptjs = require("bcryptjs");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
-usersRouter.post("/", async (request, response) => {
+usersRouter.post("/", async (request, response, next) => {
   const { username, name, password } = request.body;
+  try {
+    if (username.length < 3 || password.length < 3) {
+      return response
+        .status(400)
+        .send("username and password must be at least 3 characters long");
+    }
 
-  const existingUser = await User.findOne({ username });
-  if (existingUser) {
-    return response.status(400).json({
-      error: "username must be unique",
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return response.status(400).json({
+        error: "username must be unique",
+      });
+    }
+
+    const saltRounds = 10;
+    const passwordHash = await bcryptjs.hash(password, saltRounds);
+
+    const user = new User({
+      username,
+      name,
+      passwordHash,
     });
+
+    const savedUser = await user.save();
+
+    return response.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
   }
-
-  const saltRounds = 10;
-  const passwordHash = await bcryptjs.hash(password, saltRounds);
-
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  });
-
-  const savedUser = await user.save();
-
-  response.status(201).json(savedUser);
 });
 
 usersRouter.get("/", async (request, response) => {
@@ -33,7 +42,7 @@ usersRouter.get("/", async (request, response) => {
     url: 1,
     likes: 1,
   });
-  response.json(users);
+  return response.json(users);
 });
 
 module.exports = usersRouter;
